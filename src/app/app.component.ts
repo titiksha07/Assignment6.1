d37909112c062d9724a9a2c468d1b42c559920dd
rikrivu <30924294+rikrivu@users.noreply.github.com>
2017-08-21 23:08:39 +0530

    Assignment6.1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component , OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  constructor(){
+
+  }
+  title:string;
+  name: string;
+  
+  ngOnInit(){
+  this.title = 'Welcome'
+ this.name = 'Titiksha'
+ 
+}
+    
+  
+
 }
